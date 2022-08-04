513e47ec31e6f3e2a71eb9975c0157ccf76dde24
Hyugyu Chang <wkdgusrb3@naver.com>
2022-08-04 11:46:49 +0900

    #8 도메인 클래스 설계(게시글, 댓글)
    Article class와 ArticleComment 작성
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 위한 추가적인 표현은 추후에 업데이트 할 예정이다

---
+++ fastcampus-project-board/src/main/java/fastcampus/projectboard/domain/ArticleComment.java
@@ -0,0 +1,15 @@
+package fastcampus.projectboard.domain;
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long article_id;
+    private String content;
+    private Article article;
+    private String hashtag
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private String creatdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
