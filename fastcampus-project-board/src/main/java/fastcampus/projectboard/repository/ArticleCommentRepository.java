0338f22d2e96d0f5ce830efca8ca152dbc1d2958
Hyugyu Chang <wkdgusrb3@naver.com>
2022-09-23 09:42:43 +0900

    #2- Querydsl 지원을 이용하여 api 검색옵션 추가

--- fastcampus-project-board/src/main/java/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package fastcampus.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import fastcampus.projectboard.domain.QArticle;
+import fastcampus.projectboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import fastcampus.projectboard.domain.ArticleComment;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override//QuerydslBinderCustomizer에서 QuerydslPredicateExecutor를통한 검색 설정
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);//검색범위를 모두로 할지 아니면 특정 값으로 할지 설정 default = false
+        bindings.including(root.createdAt,root.createdBy,root.content);//id를 통한 검색 제거
+
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);//title로 검색할 때 어떻게 비교할 지 확인 여기서는 검색시 완전일치가 아닌 포함 및 대소문문자 구분 x으로 바꾸어줬다 기본은 완전일치
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);//StringExpression::containsIgnoreCase <-검색시 다르게 나옴
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    }
 }
--- fastcampus-project-board/src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
+++ fastcampus-project-board/src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,12 +1,44 @@
 package fastcampus.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import fastcampus.projectboard.domain.Article;
+import fastcampus.projectboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+/* 조회기능 추가
+public interface QuerydslPredicateExecutor<T> {
 
+  Optional<T> findById(Predicate predicate);  
+
+  Iterable<T> findAll(Predicate predicate);   
+
+  long count(Predicate predicate);            
+
+  boolean exists(Predicate predicate);        
+
+  // … more functionality omitted.
+}
+ */
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,//이걸 상속받게 해서 Article이 CRUD를 가능하게 해준다.
+        QuerydslPredicateExecutor<Article>,//,Article에 대한 검색기능 추가
+        QuerydslBinderCustomizer<QArticle>//Q클래스 사용
+{
+    @Override//QuerydslBinderCustomizer에서 QuerydslPredicateExecutor를통한 검색 설정
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);//검색범위를 모두로 할지 아니면 특정 값으로 할지 설정 default = false
+        bindings.including(root.title,root.hashtag,root.createdAt,root.createdBy,root.content);//id를 통한 검색 제거
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);//title로 검색할 때 어떻게 비교할 지 확인 여기서는 검색시 완전일치가 아닌 포함으로 바꾸어줬다 기본은 완전일치
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);//title로 검색할 때 어떻게 비교할 지 확인 여기서는 검색시 완전일치가 아닌 포함으로 바꾸어줬다 기본은 완전일치
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);//StringExpression::containsIgnoreCase <-검색시 다르게 나옴
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
